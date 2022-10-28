0b0bd2106c58bc9801a6ceb2557a067c097a5420
Ishpaul Singh <ishpaul207@gmail.com>
2022-10-28 21:58:39 +0530

    added basic packages

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="header-wrapper">
+    <div className="header-left">
+      <h2>MARKDOWN</h2>
+    </div>
+    <div className="header-right">
+      <button type="button">
+        Save Changes
+      </button>
+    </div>
+  </div>
+);
+
+export default Header;
