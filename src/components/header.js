eb70e401381b71850b80e42b60216b739fa82c4c
Ishpaul Singh <ishpaul207@gmail.com>
2022-10-28 22:54:13 +0530

    added haeder for app

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import menu from './assets/images/menu.svg';
+import logo from './assets/images/logo.svg';
+import deleteIcon from './assets/images/delete-icon.svg';
+import saveIcon from './assets/images/save-icon.svg';
+import './assets/header.css';
 
 const Header = () => (
   <div className="header-wrapper">
     <div className="header-left">
-      <h2>MARKDOWN</h2>
+      <div className="menu-btn-box">
+        <img src={menu} alt="open menu" />
+      </div>
+      <div className="header-logo">
+        <img src={logo} alt="open menu" />
+      </div>
+
     </div>
     <div className="header-right">
-      <button type="button">
+      <div className="delete-btn">
+        <img src={deleteIcon} alt="delete document" />
+      </div>
+      <button type="button" className="save-btn">
+        <img src={saveIcon} alt="save changes" />
         Save Changes
       </button>
     </div>
