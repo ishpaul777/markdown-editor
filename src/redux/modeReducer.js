acf2be9d9563abcc508ad661f27468283a9a4d47
Ishpaul Singh <ishpaul207@gmail.com>
2022-11-05 22:47:38 +0530

    added light and dark mode

--- src/redux/modeReducer.js
+++ src/redux/modeReducer.js
@@ -3,6 +3,7 @@ const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
 const modeReducer = (state = 'light', action) => {
   switch (action.type) {
     case TOGGLE_SWITCH:
+      localStorage.setItem('mode', state === 'light' ? 'dark' : 'light');
       return state === 'light' ? 'dark' : 'light';
     default:
       return state;
